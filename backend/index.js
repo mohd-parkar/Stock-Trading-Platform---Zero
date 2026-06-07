b5df70f9c3a915cf8feaa2d9839026c5814dc2b2
Mohammed Parkar <parkarmohammed74@gmail.com>
2026-06-08 01:07:30 +0530

    connect db to our backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require('dotenv').config();
+
 const app = express();
 
-app.listen(3002,()=>{
+const PORT = process.env.PORT || 3002;
+
+app.listen(PORT, async () => {
     console.log("App started");
-})
+   
+  try { // NON SRV LINK of Mongodb ATLAS WORKED
+  await mongoose.connect(process.env.ATLAS_MONGO_URL);
+  console.log("DB Connected");
+} catch (err) {
+  console.error(err);
+}
+});
