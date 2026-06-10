fee2cef0c844e97768117b8e632d7c3e943e125b
Mohammed Parkar <parkarmohammed74@gmail.com>
2026-06-10 20:07:42 +0530

    added charts using react-chart.js

--- dashboard/src/components/WatchList.js
+++ dashboard/src/components/WatchList.js
@@ -4,8 +4,38 @@ import { watchlist } from "../data/data";
 // import {Tooltip,Grow} from "@mui/material";
 import {BarChart, KeyboardArrowDown, KeyboardArrowUp, MoreHoriz} from "@mui/icons-material";
 import { Grow, Tooltip } from "@mui/material";
+import { Doughnut } from "react-chartjs-2";
+import DoughnutChart from "./DognutChart";
 
 const WatchList = () => {
+  
+  const data = {
+  labels: watchlist.map((subarray)=> subarray["name"]),
+  datasets: [
+    {
+      label: 'Price',
+      data: watchlist.map((stock)=> stock.price),
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
   return (
     <div className="watchlist-container">
       <div className="search-container">
@@ -27,6 +57,9 @@ const WatchList = () => {
           )
         })}
       </ul>
+
+      <DoughnutChart data={data}/>
+
     </div>
   );
 };
